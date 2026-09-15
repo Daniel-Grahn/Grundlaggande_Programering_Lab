0f694ffdc073b0f863726c659bacd675fae937c7
Daniel <dgrahn05@gmail.com>
2026-09-15 17:07:18 +0200

    added files

---
+++ lab1/wordfreq.py
@@ -0,0 +1,36 @@
+def validate_character(char):
+    valid_chars = ["qwertyuiopasdfghjklzxcvbnm,.0123456789"]
+    (letter == '.' )
+    pass
+
+def tokenize(document: list[str]):
+    token = []
+    for line in document:
+        # print(line.split())
+        
+        if(len(line) == 0):
+            continue
+        
+        for w in line.split():
+            word = w.lower()
+            
+            word_holder = ""
+            for i in range(0, len(word)):
+                char = word[i]
+                valid = validate_character(char)
+                
+                #valedering
+                letter = word[i]
+                
+                if (not valid):    
+                    break
+                else:
+                    word_holder+=letter
+
+            # print(word, lower_word) 
+            token.append(lower_word)
+        
+    return token
+
+
+print("result:", tokenize(['This is a simple, sentence']))
