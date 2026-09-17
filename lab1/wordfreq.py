19fa538487aca6bf2850206023121da69c42258e
Joel Johansson <joej@chalmers.se>
2026-09-17 15:01:06 +0200

    The best commit

--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -1,37 +1,44 @@
-def validate_character(char):
-    valid_chars = ["qwertyuiopasdfghjklzxcvbnm,.0123456789"]
-    (letter == '.' )
-    pass
 
-def tokenize(document: list[str]):
-    token = []
-    for line in document:
-        # print(line.split())
-        
-        if(len(line) == 0):
-            continue
-        
-        for w in line.split():
-            word = w.lower()
-            
-            word_holder = ""
-            for i in range(0, len(word)):
-                char = word[i]
-                valid = validate_character(char)
-                
-                #valedering
-                letter = word[i]
-                
-                if (not valid):    
-                    token.append(word_holder)
-                    word_holder = letter
-                    break
-                else:
-                    word_holder+=letter
+def tokenize(lines: list[str]):
+    words = []
 
-            # print(word, lower_word) 
-        
-    return token
+    for line in lines:
+        start = 0
 
+        while start < len(line):
+            while start < len(line) and line[start].isspace():
+                start += 1
 
-print("result:", tokenize(['This is a simple, sentence']))
+            if start >= len(line):
+                break
+            end = start
+
+            if line[end].isalpha():
+                while end < len(line) and line[end].isalpha():
+                    end += 1
+            elif line[end].isdigit():
+                while end < len(line) and line[end].isdigit():
+                    end += 1
+            else:
+                end += 1
+            words.append(line[start:end].lower())
+            start = end 
+    return words      
+          
+def countWords(words:list, stopWords:list) -> dict:
+  count_words_dict:dict = {}
+  word_set = set(words)
+  
+  for word in word_set:
+    if(word in stopWords):
+      continue
+    
+    count_words_dict.setdefault(word, words.count(word))
+      
+  return count_words_dict
+
+def printTopMost(frequences, n): 
+    sorted_words = sorted(frequences.items(), key=lambda x: -x[1])
+
+    for word, freq in sorted_words[:n]:
+        print(word.ljust(20) + str(freq).rjust(5))
