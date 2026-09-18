6836474c9b12c9a1d55156df9960b74c46bb5c6f
Daniel <dgrahn05@gmail.com>
2026-09-18 18:37:31 +0200

    Change the read_from_text fuction into a Generator and made a documentaion on it and typed it. I also made a function to print out the text.

--- lab1/ReadFromWeb.py
+++ lab1/ReadFromWeb.py
@@ -1,11 +1,28 @@
 import urllib.request
-import sys
+from collections.abc import Iterator
 
-def read_from_web():
-    url= "http://www.gutenberg.org/files/15/15-0.txt"
-    response = urllib.request.urlopen(url)
-    # response = urllib.request.urlopen(sys.argv[2])
-    lines = response.read().decode("utf8").splitlines()
-    print(lines)
+URL = "http://www.gutenberg.org/files/15/15-0.txt"
+def read_from_web_gen(url: str):
+    '''
+    ### This is a Text-Generator
     
-read_from_web()
+    The function takes an url as an argument 
+    than, insteed of reading the entier text 
+    it "returns" each line at a time as a string.    
+    '''
+
+    response = urllib.request.urlopen(url)
+    lines = response.read().decode("utf8").splitlines()
+    
+    for line in lines:
+        yield str(line)
+
+def print_text_from_web(url):
+    text_gen = read_from_web_gen(url)
+    for text in text_gen:
+        print(text)
+    else:
+        text_gen.close()
+        print("Done.")
+
+print_text_from_web(URL)
