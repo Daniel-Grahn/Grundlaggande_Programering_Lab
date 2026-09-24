f37d2afcfd1bb076a66e7df0e8d83ea9f0861a73
Daniel <dgrahn05@gmail.com>
2026-09-24 14:58:42 +0200

    stript each word in countWords and made a dockumentation on it

--- lab1/wordfreq.py
+++ lab1/wordfreq.py
@@ -25,17 +25,35 @@ def tokenize(lines: list[str]):
             start = end 
     return words      
           
-def countWords(words:list, stopWords:list) -> dict:
+def countWords(words:list, rawStopWords:list) -> dict:
+  """
+    ## Count how many times each word occurs in a list.
+
+    Words included in `rawStopWords` are excluded from the count. The
+    stop words are stripped of newline characters before being compared
+    with the words in `words`.
+
+    ### Args:
+        words: A list of words to count.
+        rawStopWords: A list of stop words, which may contain newline
+            characters (e.g. ``"the\\n"``).
+
+    ### Returns:
+        A dictionary mapping each word to the number of times it occurs
+        in `words`, excluding the stop words.
+    """
+  
+  stopWords = [n.strip("\n") for n in rawStopWords] 
   count_words_dict:dict = {}
+  
+  #Make a uniq list of words
   word_set = set(words)
   
   for word in word_set:
-    if(word in stopWords):
-      continue
-    
-    count_words_dict.setdefault(word, words.count(word))
+    #count the word if it is not in the stopWords
+    if not (word in stopWords):
+      count_words_dict.setdefault(word, words.count(word))
       
-
   return count_words_dict
 
 def printTopMost(frequences, n): 
